218cc202e7d6b8668c14794012ee60d7ed722b54
vipulgupta8 <147442104+vipulgupta8@users.noreply.github.com>
2023-11-26 10:57:42 +0530

    Add files via upload

---
+++ DOM/Practice projects/3-Local Time/index.js
@@ -0,0 +1,8 @@
+document.getElementById('clock');
+
+
+setInterval(function(){
+    let date = new Date()
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000)
